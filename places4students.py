75ead25331086e3f3aae735b628130644bfedaec
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-04-14 14:36:16 -0400

    places4students api call

---
+++ places4students.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+def call_places4students_api(params, cookies):
+    url = "https://www.places4students.com/Places/PropertyListings"
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    
+    response = requests.get(url, params=params, cookies=cookies, headers=headers)
+    return response
+
+
+def get_places4students_houses():
+    params = {
+        "SchoolID": "VBThwOQPAX4=",
+    }
+
+    cookies={
+        "ASP.NET_SessionId": "qmxwdtlxlffde1t3e4s0bff5",
+        "Places4StudentDisclaimer": "Agree",
+        "__AntiXsrfToken": "e24d51e2de26447ca1d222b78e0085b2"
+    }
+
+    response = call_bamboo_api(params=params, cookies=cookies)
+    return response.text
+
+response = get_places4students_houses()
+
+
