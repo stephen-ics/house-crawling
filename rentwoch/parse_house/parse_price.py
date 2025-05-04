d0a347923cecee8bfbbe56e08f0d5a73a98c050c
stephen-ics <74720279+stephen-ics@users.noreply.github.com>
2025-05-03 21:11:57 -0400

    added price parsing for rentwoch houses

---
+++ rentwoch/parse_house/parse_price.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import re
+
+def parse_price(bsObj):
+    price_text_element = bsObj.find("div", class_="price_box")
+
+    if not price_text_element:
+        return None
+
+    price_text = price_text_element.text
+    price = format_price(price_text)
+
+    return price
+
+def format_price(price_str):
+    price_str_match = re.search(r"\$(\d+)", price_str)
+
+    if price_str_match == None:
+        return None
+
+    price = price_str_match.group(1)
+
+    return price
